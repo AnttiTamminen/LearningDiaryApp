5e76b942cfbd949e7b0d9468377ec518e6f9f16f
Antti <antti.a.tamminen@gmail.com>
2022-07-26 18:00:55 +0300

    Added show infotext

--- LearningDiaryApp/wwwroot/js/site.js
+++ LearningDiaryApp/wwwroot/js/site.js
@@ -1,4 +1,11 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function showInfo() {
+    document.getElementById("maini").style.display = "none";
 
-// Write your JavaScript code.
+    document.getElementById("infoText").style.display = "block";
+}
+
+function hideInfo() {
+    document.getElementById("maini").style.display = "block";
+
+    document.getElementById("infoText").style.display = "none";
+}
