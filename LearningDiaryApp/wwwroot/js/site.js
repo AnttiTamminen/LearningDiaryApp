4fc514c0a665bf51a4c1e9c7cd4585bb093642d3
Antti <antti.a.tamminen@gmail.com>
2022-08-03 15:02:21 +0300

    Few minor fixes

--- LearningDiaryApp/wwwroot/js/site.js
+++ LearningDiaryApp/wwwroot/js/site.js
@@ -1,10 +1,8 @@
 ﻿function showInfo() {
-    document.getElementById("maini").style.display = "none";
     document.getElementById("infoText").style.display = "block";
 }
 
 function hideInfo() {
-    document.getElementById("maini").style.display = "block";
     document.getElementById("infoText").style.display = "none";
 }
 
