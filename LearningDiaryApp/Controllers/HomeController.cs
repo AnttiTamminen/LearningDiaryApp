297d7745bb07241de23877b1eafef5f71a5e26fc
Antti <antti.a.tamminen@gmail.com>
2022-08-05 14:25:03 +0300

    Finished search functionality

--- LearningDiaryApp/Controllers/HomeController.cs
+++ LearningDiaryApp/Controllers/HomeController.cs
@@ -37,59 +37,124 @@ namespace LearningDiaryApp.Controllers
 
         public async Task<IActionResult> TopicSearchResult(string searchTerm)
         {
-            var result = Query.Search(searchTerm, await _context.Topic.ToListAsync());
-            ViewModel mymodel = new ViewModel();
-            mymodel.Topics = result;
-            mymodel.Tasks = await _context.Task.ToListAsync();
-            mymodel.Notes = await _context.Note.ToListAsync();
-            return View("Index", mymodel);
+            if (!String.IsNullOrEmpty(searchTerm))
+            {
+                var result = Query.Search(searchTerm, await _context.Topic.ToListAsync());
+                ViewModel mymodel = new ViewModel();
+                mymodel.Topics = result;
+                mymodel.Tasks = await _context.Task.ToListAsync();
+                mymodel.Notes = await _context.Note.ToListAsync();
+                return View("Index", mymodel);
+            }
+            return View("Index", new ViewModel());
         }
 
         public async Task<IActionResult> TaskSearchResult(string searchTerm)
         {
-            var result = Query.Search(searchTerm, await _context.Task.ToListAsync());
-            var tIds = new List<int>();
-            var taskiId = new List<int>();
-            foreach (var task in result)
+            if (!String.IsNullOrEmpty(searchTerm))
             {
-                tIds.Add(task.TopicId);
-                taskiId.Add(task.Id);
-            }
+                var result = Query.Search(searchTerm, await _context.Task.ToListAsync());
 
-            var topicsFiltered = new List<Topic>();
-            var topics = await _context.Topic.ToListAsync();
-            foreach (var id in tIds.Distinct())
-            {
-                var helpList = Query.Search(id.ToString(), topics);
-                topicsFiltered.Add(helpList[0]);
-            }
-
-
-            foreach (var topi in topicsFiltered)
-            {
-                for (int i = topi.Tasks.Count - 1; i >= 0; i--)
+                var topicIds = new List<int>();
+                var taskiIds = new List<int>();
+                foreach (var task in result)
                 {
-                    if (!taskiId.Contains(topi.Tasks.ToList()[i].Id))
+                    topicIds.Add(task.TopicId);
+                    taskiIds.Add(task.Id);
+                }
+
+                var topicsFiltered = new List<Topic>();
+                var topics = await _context.Topic.ToListAsync();
+                foreach (var id in topicIds.Distinct())
+                {
+                    var helpList = Query.Search(id.ToString(), topics);
+                    topicsFiltered.Add(helpList.First());
+                }
+
+
+                foreach (var topi in topicsFiltered)
+                {
+                    for (int i = topi.Tasks.Count - 1; i >= 0; i--)
                     {
-                        topi.Tasks.Remove(topi.Tasks.ElementAt(i));
+                        if (!taskiIds.Contains(topi.Tasks.ToList()[i].Id))
+                            topi.Tasks.Remove(topi.Tasks.ElementAt(i));
                     }
                 }
-            }
 
-            ViewModel mymodel = new ViewModel();
-            mymodel.Topics = topicsFiltered;
-            mymodel.Tasks = result;
-            mymodel.Notes = await _context.Note.ToListAsync();
-            return View("Index", mymodel);
+                ViewModel mymodel = new ViewModel();
+                mymodel.Topics = topicsFiltered;
+                mymodel.Tasks = result;
+                mymodel.Notes = await _context.Note.ToListAsync();
+                return View("Index", mymodel);
+            }
+            return View("Index", new ViewModel());
         }
+            
 
         public async Task<IActionResult> NoteSearchResult(string searchTerm)
         {
-            ViewModel mymodel = new ViewModel();
-            mymodel.Topics = await _context.Topic.ToListAsync();
-            mymodel.Tasks = await _context.Task.ToListAsync();
-            mymodel.Notes = await _context.Note.ToListAsync();
-            return View("Index", mymodel);
+            if (!String.IsNullOrEmpty(searchTerm))
+            {
+                var result = Query.Search(searchTerm, await _context.Note.ToListAsync());
+
+                var topicIds = new List<int>();
+                var taskiIds = new List<int>();
+                var noteIds = new List<int>();
+
+
+                foreach (Note note in result)
+                {
+                    taskiIds.Add(note.TaskId);
+                    noteIds.Add(note.Id);
+                }
+
+                var tasksFiltered = new List<Models.Task>();
+                var tasks = await _context.Task.ToListAsync();
+                foreach (var id in taskiIds.Distinct())
+                {
+                    var helpList1 = Query.Search(id.ToString(), tasks);
+                    tasksFiltered.Add(helpList1.First());
+                }
+
+                foreach (var taski in tasksFiltered)
+                {
+                    for (int i = taski.Notes.Count - 1; i >= 0; i--)
+                    {
+                        if (!noteIds.Contains(taski.Notes.ToList()[i].Id))
+                            taski.Notes.Remove(taski.Notes.ElementAt(i));
+                    }
+                }
+
+                foreach (Models.Task task in tasksFiltered)
+                {
+                    topicIds.Add(task.TopicId);
+                }
+
+                var topicsFiltered = new List<Topic>();
+                var topics = await _context.Topic.ToListAsync();
+                foreach (var id in topicIds.Distinct())
+                {
+                    var helpList = Query.Search(id.ToString(), topics);
+                    topicsFiltered.Add(helpList.First());
+                }
+
+
+                foreach (var topi in topicsFiltered)
+                {
+                    for (int i = topi.Tasks.Count - 1; i >= 0; i--)
+                    {
+                        if (!taskiIds.Contains(topi.Tasks.ToList()[i].Id))
+                            topi.Tasks.Remove(topi.Tasks.ElementAt(i));
+                    }
+                }
+
+                ViewModel mymodel = new ViewModel();
+                mymodel.Topics = topicsFiltered;
+                mymodel.Tasks = tasksFiltered;
+                mymodel.Notes = result;
+                return View("Index", mymodel);
+            }
+            return View("Index", new ViewModel());
         }
 
         public IActionResult Privacy()
