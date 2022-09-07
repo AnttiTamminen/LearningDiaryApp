c0af3b74d2967370c9bd5442e16708c861c42553
Antti Tamminen <antti.a.tamminen@gmail.com>
2022-09-07 16:52:21 +0300

    removed login

--- LearningDiaryApp/wwwroot/js/site.js
+++ LearningDiaryApp/wwwroot/js/site.js
@@ -8,29 +8,6 @@
     }
 }
 
-function loginPrompt() {
-    var password = sessionStorage.getItem("passKey");
-    if (password != "asd") {  //password is showing in browser debugging window -> to database?
-        document.getElementById("allCont").style.display = "none";
-        document.getElementById("login").style.display = "block";
-    } else {
-        document.getElementById("allCont").style.display = "block";
-        document.getElementById("login").style.display = "none";
-    }
-}
-
-function setPassword() {
-    var password = document.forms[0].elements["logg"].value;
-    sessionStorage.setItem("passKey", password);
-    document.forms[0].reset();
-    loginPrompt();
-}
-
-function logout() {
-    sessionStorage.setItem("passKey", "password");
-    loginPrompt();
-}
-
 function sortTopics(option) {
     var list = document.getElementById('allTop');
     var items = list.childNodes;
